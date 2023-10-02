5dd957357d7449d7adebd6fd98358e3a2914caec
mahibhuiyan <70013128+mahibhuiyan@users.noreply.github.com>
2023-10-03 02:25:09 +0600

    #

--- assets/product-custom.js
+++ assets/product-custom.js
@@ -1,6 +1,6 @@
  
 // all thumbnails  
-const thumbnailItems = document.querySelectorAll('.thumbnail-list__item slider__slide');
+const thumbnailItems = document.querySelectorAll('.thumbnail-list__item');
 
 //  mouseover event listener to each of the thumbnail items
 thumbnailItems.forEach(item => {
